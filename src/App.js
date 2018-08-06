42ddc1fa548e03b1a78a26b0c1ad06857c8cd2f7
MarkJunHao <MarkJunHao@163.com>
2018-08-06 18:32:52 +0800

    add trip

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import {BrowserRouter as Router,Route,Link,Redirect,Switch} from 'react-router-d
 import logo from './logo.svg';
 import './App.css';
 import './font/iconfont.css'
+import $ from 'jquery'
+
 
 import Custom from './components/custom'
 import Home from './components/home'
@@ -21,25 +23,31 @@ import Hotel from './components/hotel'
 
 
 class App extends Component {
+		tap(){
+  		window.location.reload()
+  	}
+	
   render() {
     return (
       <div>
       	<Router>
       		<div>
 						<div className='home_nav'>
-	      		<Link to='/home'>首页</Link>
-	      		<Link to='/trip'>主题旅行</Link>
-	      		<Link to='/hotel'>主题酒店</Link>
-	      		<Link to='/custom'>私人订制</Link>
-	      		<Link to='/vr'>vr旅行</Link>
-	      		<Link to='/impression'>主题印象</Link>
-	      		<Link to='/heart'>零感触发</Link>
-	      		<Link to='/register'>注册/</Link>
-	      		<Link to='/login'>登录</Link>
+	      		<Link to='/home' onClick={this.tap.bind(this)}>首页</Link>
+	      		<Link to='/trip' onClick={this.tap.bind(this)}>主题旅行</Link>
+	      		<Link to='/hotel' onClick={this.tap.bind(this)}>主题酒店</Link>
+	      		<Link to='/custom' onClick={this.tap.bind(this)}>私人订制</Link>
+	      		<Link to='/vr' onClick={this.tap.bind(this)}>vr旅行</Link>
+	      		<Link to='/impression' onClick={this.tap.bind(this)}>主题印象</Link>
+	      		<Link to='/heart' onClick={this.tap.bind(this)}>零感触发</Link>
+	      		<Link to='/register' onClick={this.tap.bind(this) }>注册/</Link>
+	      		<Link to='/login' onClick={this.tap.bind(this)}>登录</Link>
 						</div>
-						
+					</div>	
+				</Router>
 						<hr/>
-						
+				<Router>
+      		<div>		
 						<Switch>
 						<Route path='/home' component={Home}></Route>
 						<Route path='/trip' component={Trip}></Route>
@@ -47,7 +55,6 @@ class App extends Component {
 						<Route path='/custom' component={Custom}></Route>
 						<Route path='/vr' component={Vr}></Route>
 						<Route path='/impression' component={Impression}></Route>
-						
 						<Route path='/heart' component={Heart}></Route>
 						<Route path='/register' component={Register}></Route>
 						<Route path='/login' component={Login}></Route>
