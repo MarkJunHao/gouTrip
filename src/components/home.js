a2ce8d30bf0623f09a9bf77fc19f9cba73a1e9ba
qiaolei <tsql333@126.com>
2018-08-07 09:26:02 +0800

    aaaa

--- src/components/home.js
+++ src/components/home.js
@@ -1,10 +1,5 @@
 import React from 'react'
-
-<<<<<<< HEAD
 import home_01 from '../images/home_01.png'
-=======
->>>>>>> ff0b00f689e45f1362a16a9fbdf361f2ac79f31c
-
 class Home extends React.Component{
 	
 	render(){
@@ -12,12 +7,9 @@ class Home extends React.Component{
 			<div>
 		
 			<div className='app_con'>
-<<<<<<< HEAD
 				<img src={home_01} alt='logo'/>
 			
 			</div>
-=======
->>>>>>> ff0b00f689e45f1362a16a9fbdf361f2ac79f31c
 			
 			</div>
 		)
