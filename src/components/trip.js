a1e54e7ea402eed7c068cee6ba92a9ad15ff3f6d
MarkJunHao <MarkJunHao@163.com>
2018-08-07 09:14:21 +0800

    add trip list

--- src/components/trip.js
+++ src/components/trip.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import '../css/trip.css'
-import { Menu, Dropdown, Icon, message } from 'antd';
+import 'antd/dist/antd.css';
+import { Menu, Dropdown, Icon, message,Pagination } from 'antd';
+import $ from 'jquery'
+import '../data/trip_list.js'
 
 import bgImage from '../images/b3c257f1a5c5d48457ca788a011039.jpg'
 import bgImage2 from '../images/2.png'
@@ -11,10 +14,34 @@ import bgImage4 from '../images/trip-4.jpg'
 
 class Trip extends React.Component{
 	constructor (props){
-		super(props);
+		super(props),
+		this.state={
+			arr:[]
+		}
 		
-		
-		
+	}
+	
+
+	
+	componentDidMount(){
+		var _this = this
+		$.ajax({
+			type:'get',
+			url:'http://http://www.goutrip.com/trip',
+			dataType:'json',
+			success:function(data){
+				//console.log(data)
+			
+				_this.setState({arr:data.list})
+				console.log(_this.state.arr)
+//				_this.state.arr.map(function(item,i){
+//					return (
+//						console.log(item)
+//					)
+//					
+//				})
+			}
+		})
 	}
 	
 	render(){
@@ -117,18 +144,75 @@ class Trip extends React.Component{
 							<div className='rigBar'>
 								<div className='sec1'>
 									旅行方式&nbsp;
-									<i class='sec-arrow-1' id='sec-arr'></i>
-									<div>
+									<i className='sec-arrow-1' id='sec-arr'></i>
+									<div className='list2'>
+										<a className='J_group' href='#' type='groupType' data-id='1'>独立团</a>
+										<a className='J_group' href='#' type='groupType' data-id='2'>自由行</a>
+										<a className='J_group' href='#' type='groupType' data-id='3'>半自由</a>
+										<a className='J_group' href='#' type='groupType' data-id='4'>私家团</a>
 										
 									</div>
 								</div>
 								<div className='selectParm'>
-								
+									<a className='J_all' href='#' type='sellWell' data-id='0'>全部</a>
+									<a className='J_hot' href='#' type='sellWell' data-id='1'>热门</a>
+									<span className='J_day time1' type='sort'>天数</span>
+									<p>
+										<i className='img_top1' data-id = '5'></i>
+										<i className='img_botm1' data-id = '6'></i>
+									</p>
+									<span className='J_price time2' type='sort'>价格</span>
+									
+									<p>
+										<i className='img_top2' data-id = '3'></i>
+										<i className='img_botm2' data-id = '4'></i>
+									</p>
 								</div>
 							</div>
 							
+							{/*主题产品列表*/}
 							
 							
+								
+								
+									<div className="J_list">
+										<div className="connect">
+											{/*每一小块内容*/}
+										
+											{
+												this.state.arr.map(function(item,i){
+												return(
+													
+													<div key={i}>
+														<a href='#' target='_blank'>
+															<img className ='l_img1' src={item.img}/>
+														</a>
+														<p>
+															<a href='#' target='_blank'>{item.title}</a>
+														</p>
+														<p> 
+															<span className='money'>
+																<strong>{item.price}&nbsp;</strong>元起/人
+															</span>
+															<span className='address'>
+																<span className='mapIcon'></span>
+																&nbsp;出发地 : {item.address}
+															</span>
+														</p>
+													</div>
+													
+													)
+												})
+												
+											}
+										</div>
+									</div>
+								
+							
+							{
+								<Pagination defaultCurrent={1} total={this.state.arr.length} defaultPageSize={8} pageSize={8}/>
+							} 
+						
 						</div>
 						{/*主题右侧结束*/}
 					</div>
