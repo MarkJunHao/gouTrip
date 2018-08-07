58e1892698594aa79e78fb59ff21944b888bd33b
qiaolei <tsql333@126.com>
2018-08-07 09:20:53 +0800

    new app

--- src/App.js
+++ src/App.js
@@ -13,17 +13,12 @@ import Impression from './components/impression'
 import Register from './components/register'
 import Hotel from './components/hotel'
 
-<<<<<<< HEAD
+
 import weibo from './images/weibo_xl.png';
 import baijia from './images/baijia.png';
 import toutiao from './images/toutiao.png';
 import weixin from './images/appGoutrip.png';
-//var active={
-//	color:'rgb(242, 175, 51)'
-//}
-=======
 
->>>>>>> ff0b00f689e45f1362a16a9fbdf361f2ac79f31c
 class App extends Component {
 	
 	
@@ -35,7 +30,6 @@ class App extends Component {
       		<div className='nav_wrap'>
 						<div className='home_nav'>
 						<ul>
-<<<<<<< HEAD
 							<li><span className='logopic'></span></li>
 		      		<li><NavLink to='/home' activeStyle={{color:'rgb(242, 175, 51)'}}>首页</NavLink></li>
 		      		<li className='nav_sub'>
@@ -56,19 +50,6 @@ class App extends Component {
 		      		<span className='iconfont icon-gerenzhongxin'></span>
 		      		<Link to='/register'>注册</Link>
 		      		<a href='javascript:void(0)'>/</a>
-=======
-							<li><a href='/'></a></li>
-		      		<li><Link to='/home'>首页</Link></li>
-		      		<li><Link to='/trip'>主题旅行</Link></li>
-		      		<li><Link to='/hotel'>主题酒店</Link></li>
-		      		<li><Link to='/custom'>私人订制</Link></li>
-		      		<li><Link to='/vr'>vr旅行</Link></li>
-		      		<li><Link to='/impression'>主题印象</Link></li>
-		      		<li><Link to='/heart'>零感触发</Link></li>
-		      	</ul>
-		      	<div className='home_nav_right'>
-		      		<Link to='/register'>注册</Link>
->>>>>>> ff0b00f689e45f1362a16a9fbdf361f2ac79f31c
 		      		<Link to='/login'>登录</Link>
 						</div>
 						</div>
@@ -147,9 +128,5 @@ class App extends Component {
 		 });
 	}
 }
-<<<<<<< HEAD
-export default App;
-=======
 
 export default App;
->>>>>>> ff0b00f689e45f1362a16a9fbdf361f2ac79f31c
