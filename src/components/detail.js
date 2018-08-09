98f33ba0c8f7d93731ab3ab27d75222b94884e3f
qiaolei <tsql333@126.com>
2018-08-09 17:07:12 +0800

    detail 1.0

--- src/components/detail.js
+++ src/components/detail.js
@@ -1,9 +1,13 @@
-import React from 'react'
-
+import React from 'react';
+import $ from 'jquery';
+import Mock from 'mockjs';
+import '../css/detail.css';
 
 class Detail extends React.Component{
 	
-	
+	componentDidMount(){
+		console.log(this.props.location.query);
+	}
 	render(){
 		return(
 			
